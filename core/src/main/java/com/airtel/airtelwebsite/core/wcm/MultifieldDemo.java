f5e6de21b194fe7ac3c3ee0d12979a0fba132b75
F600155 <abinay.pingili@kp.org>
2020-03-09 23:41:20 -0400

    added multifield and sightly components

---
+++ core/src/main/java/com/airtel/airtelwebsite/core/wcm/MultifieldDemo.java
@@ -0,0 +1,77 @@
+package com.airtel.airtelwebsite.core.wcm;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+import javax.jcr.PathNotFoundException;
+import javax.jcr.RepositoryException;
+import javax.jcr.ValueFormatException;
+
+import org.apache.sling.api.resource.Resource;
+import com.adobe.cq.sightly.WCMUsePojo;
+import com.airtel.airtelwebsite.wcm.beans.MultifieldItem;
+
+public class MultifieldDemo extends WCMUsePojo {
+
+	private List<MultifieldItem> multifieldItems = new ArrayList<>();
+	
+	@Override
+	public void activate() throws Exception {
+		
+		//populateListUsingSling();
+		populateListUsingJCR();
+
+	}
+
+	private void populateListUsingJCR() throws PathNotFoundException, ValueFormatException, RepositoryException {
+		
+		Node multiFieldParentNode = getResource().adaptTo(Node.class);
+		if(multiFieldParentNode.hasNode("multifield")) {
+			Node multiFieldNode = multiFieldParentNode.getNode("multifield");
+			if(multiFieldNode.hasNodes()) {
+				NodeIterator multiFieldNodes = multiFieldNode.getNodes();
+				while(multiFieldNodes.hasNext()) {
+					MultifieldItem multiFieldItem = new MultifieldItem();
+					Node itemNode = multiFieldNodes.nextNode();
+					String title = itemNode.getProperty("title").getString();
+					String description = itemNode.getProperty("description").getString();
+					String imagepath = itemNode.getProperty("imagepath").getString();
+					
+					multiFieldItem.setTitle(title.toUpperCase());
+					multiFieldItem.setDescription(description.toUpperCase());
+					multiFieldItem.setImagepath(imagepath);
+					multifieldItems.add(multiFieldItem);
+				}
+			}
+		}
+		
+	}
+
+	private void populateListUsingSling() {
+		
+		Resource multiField = getResource().getChild("multifield");
+		Iterator<Resource> resourceItems = multiField.listChildren();
+		
+		while(resourceItems.hasNext()) {
+			MultifieldItem multiFieldItem = new MultifieldItem();
+			Resource childNode = resourceItems.next();
+			String title = childNode.getValueMap().get("title", String.class);
+			String desc = childNode.getValueMap().get("description", String.class);
+			String imagePath = childNode.getValueMap().get("imagepath", String.class);
+			
+			multiFieldItem.setTitle(title.toUpperCase());
+			multiFieldItem.setDescription(desc.toUpperCase());
+			multiFieldItem.setImagepath(imagePath);
+			multifieldItems.add(multiFieldItem);
+		}
+		
+	}
+	
+	public List<MultifieldItem> getMultifieldItems() {
+		return multifieldItems;
+	}
+
+}
---
+++ core/src/main/java/com/airtel/airtelwebsite/wcm/beans/MultifieldItem.java
@@ -0,0 +1,26 @@
+package com.airtel.airtelwebsite.wcm.beans;
+
+public class MultifieldItem {
+	private String title;
+	private String description;
+	private String imagepath;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getImagepath() {
+		return imagepath;
+	}
+	public void setImagepath(String imagepath) {
+		this.imagepath = imagepath;
+	}
+}
